476c58d5bf5d6525839889e9c6d8c577b45e632c
Jordan Trevino <jordan.trevino@gmail.com>
2013-07-23 16:39:23 -0400

    customize student views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
 ActiveRecord::Schema.define(:version => 20130723155730) do
 
   create_table "answers", :force => true do |t|
