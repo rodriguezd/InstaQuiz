ced334cc1a598e85d484151271ef00ad7777154f
Kirin Masood <kirinmasood@gmail.com>
2013-07-24 15:52:08 -0400

    view fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-  	@current_user ||= User.find(session[:user_id])
+  	@current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
   def logo_link
