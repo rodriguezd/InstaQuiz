662f5533ce25421f1e0266b1c2c5a0e919aaca6d
David Rodriguez <davidrodriguez212@gmail.com>
2013-08-02 10:56:30 -0400

    More validations including checking role selected for new user

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,8 +16,10 @@ class User < ActiveRecord::Base
   validates_presence_of :name
   validates_uniqueness_of :email
 
+  validates_presence_of :email
   validates_confirmation_of :email
   validates_presence_of :email_confirmation
+  validates_presence_of :password
   validates_confirmation_of :password
   validates_presence_of :password_confirmation
 
