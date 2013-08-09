b66beafc2d90af9c1941685aeae10168e866820d
Jordan Trevino <jordan.trevino@gmail.com>
2013-08-09 13:48:27 -0400

    routes completing a quiz to the overview page

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -225,9 +225,8 @@ class QuizzesController < ApplicationController
         # @quiz.take_email   ###
         redirect_to results_quiz_path(@quiz) and return
       elsif params[:status] == "completed"
-        redirect_to results_quiz_path(@quiz) and return
+        redirect_to overview_quiz_path(@quiz) and return
       end
-
       redirect_to dashboard_user_path(current_user) and return
     elsif current_user.role?(:student)
       format.html { redirect_to dashboard_user_path(current_user), notice: 'You do not have the privileges to change the status of this quiz.' }
